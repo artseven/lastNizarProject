7ef67b9aa53f0c56c0a8b3c964774f04be5192b8
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-15 12:53:25 -0400

    Card-routes

--- app.js
+++ app.js
@@ -51,6 +51,9 @@ app.use('/', myAuthStuff);
 
 const myListStuff = require('./routes/list-api-routes');
 app.use('/', myListStuff);
+
+const myCardStuff = require('./routes/card-api-routes');
+app.use('/', myCardStuff);
 // -----------------------------------------------------------------------------
 
 
---
+++ routes/card-api-routes.js
@@ -0,0 +1,41 @@
+const ensureLoggedInApiVersion = require('../lib/ensure-logged-in-api-version');
+const ListModel = require('../models/list-model');
+const CardModel = require('../models/card-model');
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/lists/:id/cards', ensureLoggedInApiVersion, (req, res, next) => {
+  CardModel
+  .findOne({ owner: req.user._id})
+  // -1 means opposite order
+  .sort({ position: -1})
+  //Tell mongo to execute query
+  .exec((err, lastCard) => {
+    if (err) {
+      res.status(500).json({ message: 'Find List went to shit'});
+      return;
+    }
+    //Defaults to 1 if the er are no cards (new user)
+    let newPosition = 1;
+
+    if(lastCard) {
+      //but use the last card's position (+1) if we have one
+      newPosition = lastCard.position +1;
+    }
+
+    const theCard = new CardModel({
+      title: req.body.cardTitle,
+      position: newPosition,
+      list: req.params.id
+    });
+
+    theCard.save((err) => {
+      if (err) {
+        res.status(500).json({ message: "Card save went to shit"});
+      }
+
+      res.status(200).json(theCard);
+    });
+  });//Close exec callback
+});//Close post('/lists/:id/cards')
